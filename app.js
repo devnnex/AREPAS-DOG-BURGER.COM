24538912d86c1000d3b101ed85efe6ed96b0e5de
devnnex <tecnometaversesolutions@gmail.com>
2025-11-27 22:37:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1143,72 +1143,72 @@ const products = [
   category: 'gaseosas',
   title: 'Postobon',
   price: 5000,
-  desc: 'Refrescante Coca-Cola en botella de 400 mililitros.',
+  desc: 'Refrescante Postobón sabor tradicional, disponible en presentaciones personales y familiares.',
   image: 'images/gaseosa1.png',
-  ingredients: ['Coca-Cola 400ml']
+  ingredients: ['Postobón 400ml']
 },
 {
   id: 'g2',
   category: 'gaseosas',
   title: 'Sprite',
   price: 9000,
-  desc: 'Botella familiar de Coca-Cola de 1.5 litros.',
+  desc: 'Clásica Sprite con su sabor cítrico único, disponible en presentaciones personales y familiares.',
   image: 'images/gaseosa2.png',
-  ingredients: ['Coca-Cola 1.5L']
+  ingredients: ['Sprite 1.5L']
 },
 {
   id: 'g3',
   category: 'gaseosas',
   title: 'Quatro',
   price: 5000,
-  desc: 'Refrescante Pepsi individual de 400 mililitros.',
+  desc: 'Refrescante Quatro con su toque cítrico natural, disponible en presentaciones personales y familiares.',
   image: 'images/gaseosa3.png',
-  ingredients: ['Pepsi 400ml']
+  ingredients: ['Quatro 400ml']
 },
 {
   id: 'g4',
   category: 'gaseosas',
   title: 'Coca-Cola',
   price: 9000,
-  desc: 'Botella grande de Pepsi para compartir.',
+  desc: 'Tradicional Coca-Cola para acompañar cualquier comida, disponible en presentaciones personales y familiares.',
   image: 'images/gaseosa4.png',
-  ingredients: ['Pepsi 1.5L']
+  ingredients: ['Coca-Cola 1.5L']
 },
 {
   id: 'g5',
   category: 'gaseosas',
   title: 'Bretaña',
   price: 5000,
-  desc: 'Deliciosa Manzana Postobón en presentación de 400 mililitros.',
+  desc: 'Clásica Bretaña, refrescante y equilibrada, disponible en presentaciones personales y familiares.',
   image: 'images/gaseosa5.png',
-  ingredients: ['Manzana Postobón 400ml']
+  ingredients: ['Bretaña 400ml']
 },
 {
   id: 'g6',
   category: 'gaseosas',
   title: 'H2O',
   price: 9000,
-  desc: 'Manzana Postobón familiar de 1.5 litros.',
+  desc: 'Deliciosa H2O saborizada, perfecta para refrescarte, disponible en presentaciones personales y familiares.',
   image: 'images/gaseosa6.png',
-  ingredients: ['Manzana Postobón 1.5L']
+  ingredients: ['H2O 1.5L']
 },
 {
   id: 'g7',
   category: 'gaseosas',
   title: 'Hit',
   price: 5000,
-  desc: 'Refrescante Sprite con su sabor cítrico único.',
+  desc: 'Dulce y refrescante jugo Hit, disponible en presentaciones personales y familiares.',
   image: 'images/gaseosa7.png',
-  ingredients: ['Sprite 400ml']
+  ingredients: ['Hit 400ml']
 },
 {
   id: 'g8',
   category: 'gaseosas',
   title: 'Premio',
   price: 5000,
-  desc: 'Tradicional Colombiana en presentación individual.',
+  desc: 'Refrescante gaseosa Premio, disponible en presentaciones personales y familiares.',
   image: 'images/gaseosa8.png',
-  ingredients: ['Colombiana 400ml']
+  ingredients: ['Premio 400ml']
 },
 
 //MALTEADAS
